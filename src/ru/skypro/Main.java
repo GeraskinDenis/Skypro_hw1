b5a532c6245082ea67247d6035d7c96a1e605b0c
Geraskin Denis <fresh7tea@gmail.com>
2022-01-26 20:02:21 +0300

    Задача 1

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,16 @@
+package ru.skypro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Задача 1
+        // Целые числа
+        byte vByte = 12;
+        short vShort = 200;
+        int vInt = 99_999;
+        long vLong = 99_999_999L;
+        // Дробные числа
+        float vFloat = 0.123F;
+        double vDouble = 10.45;
+    }
+}
