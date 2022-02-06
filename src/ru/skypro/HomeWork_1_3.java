fb40ec1ffeec09cbe0fee1f7d85c54c9feebcc45
Geraskin Denis <fresh7tea@gmail.com>
2022-02-06 03:27:00 +0300

    HW 1.3 and 1.4 refactoring

--- src/ru/skypro/HomeWork_1_3.java
+++ src/ru/skypro/HomeWork_1_3.java
@@ -4,6 +4,11 @@ import javax.crypto.spec.PSource;
 
 public class HomeWork_1_3 {
     public static void main(String[] args) {
+        /**
+         * 1.3 УСЛОВНЫЕ ОПЕРАТОРЫ
+         */
+
+
         // Задание 1
         {
             System.out.println("\nЗадание №1");
@@ -88,6 +93,64 @@ public class HomeWork_1_3 {
                     season = "ERROR: The month number is incorrect.";
             }
             System.out.println(season);
+        }
+
+        // Задание 6
+        {
+            System.out.println("\nЗадание 6");
+            int age = 19;
+            int salary = 100_000;
+            float result = 0;
+
+            if (age < 23) {
+                result = salary * 2;
+            } else if (age >= 23) {
+                result = salary * 3;
+            }
+
+            if (salary >= 80_000) {
+                result *= 1.2F;
+            } else if (salary >= 50_000) {
+                result *= 1.5F;
+            }
+            System.out.printf("Мы готовы выдать вам кредитную карту с лимитом %.2f рублей\n", result);
+        }
+
+        // Задание 7
+        {
+            System.out.println("\nЗадание 7");
+            // Входные параметры
+            float rate = 10;
+            int loanTermInMonths = 12;
+            int maximumPercentageOfSalary = 50;
+            int age = 25;
+            int salary = 60_000;
+            int wantedSum = 330_000;
+            boolean loanApproved;
+
+            // Находим ставку
+            if (age <= 23) {
+                rate += 1;
+            } else if (age < 30) {
+                rate += 0.5F;
+            }
+
+            if (salary > 80_000) {
+                rate -= 0.7;
+            }
+
+            // Расчетаем первый платёж по кредиту
+            double loanBodyPerMonth = wantedSum / loanTermInMonths;
+            double interestAmount = wantedSum * rate / loanTermInMonths / 100;
+            double firstPayment = loanBodyPerMonth + interestAmount;
+
+            // Расчетаем максимальный платёж по кредиту
+            float maxPayment = salary * maximumPercentageOfSalary / 100;
+            loanApproved = maxPayment > firstPayment;
+
+            // Выводим результат
+            System.out.printf("Максимальный платеж при ЗП %d равен %.2f рублей. Платеж по кредиту %.2f рублей. "
+                    + ((loanApproved) ? "Одобрено." : "Отказано.") + "\n", salary, maxPayment, firstPayment);
         }
     }
 }
--- src/ru/skypro/HomeWork_1_4.java
+++ src/ru/skypro/HomeWork_1_4.java
@@ -2,6 +2,10 @@ package ru.skypro;
 
 public class HomeWork_1_4 {
     public static void main(String[] args) {
+        /**
+         *
+         */
+
         // Задание 1
         {
             System.out.println("\nЗадание 1");
@@ -42,13 +46,14 @@ public class HomeWork_1_4 {
             System.out.println("\nЗадание 4");
             for (int i = 1; i <= 30; i++) {
                 System.out.print(i + ": ");
-                if (i % 3 == 0) {
-                    System.out.println("ping");
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    System.out.print("ping pong");
+                } else if (i % 3 == 0) {
+                    System.out.print("ping");
                 } else if (i % 5 == 0) {
-                    System.out.println("pong");
-                } else {
-                    System.out.println();
+                    System.out.print("pong");
                 }
+                System.out.println();
             }
         }
         // Задание 5
@@ -56,71 +61,11 @@ public class HomeWork_1_4 {
             System.out.println("\nЗадание 5");
             int x = 0;
             int y = 1;
-            System.out.print(x + " " + y);
-            for (int i = 2; i < 10; i++) {
-                if (i % 2 == 0) {
-                    x += y;
-                } else {
-                    y += x;
-                }
-                System.out.print(" " + Math.max(x, y));
+            for (int i = 0; i < 5; i++) {
+                System.out.print(x + " " + y + " ");
+                x += y;
+                y +=x;
             }
-        }
-        // Задание 6
-        {
-            System.out.println("\nЗадание 6");
-            int age = 19;
-            int salary = 58_000;
-            float result = 0;
-
-            if (age < 23) {
-                result = salary * 2;
-            } else if (age >= 23) {
-                result = salary * 3;
-            }
-
-            if (salary >= 80_000) {
-                result *= 1.2F;
-            } else if (salary >= 50_000) {
-                result *= 1.5F;
-            }
-            System.out.printf("Мы готовы выдать вам кредитную карту с лимитом %.2f рублей\n", result);
-        }
-        // Задание 7
-        {
-            System.out.println("\nЗадание 7");
-            // Входные параметры
-            float rate = 10;
-            int loanTermInMonths = 12;
-            int maximumPercentageOfSalary = 50;
-            int age = 25;
-            int salary = 60_000;
-            int wantedSum = 330_000;
-            boolean loanApproved;
-
-            // Находим ставку
-            if (age <= 23) {
-                rate += 1;
-            } else if (age < 30) {
-                rate += 0.5F;
-            }
-
-            if (salary > 80_000) {
-                rate -= 0.7;
-            }
-
-            // Расчетаем первый платёж по кредиту
-            double loanBodyPerMonth = wantedSum / loanTermInMonths;
-            double interestAmount = wantedSum * rate / loanTermInMonths / 100;
-            double firstPayment = loanBodyPerMonth + interestAmount;
-
-            // Расчетаем максимальный платёж по кредиту
-            float maxPayment = salary * maximumPercentageOfSalary / 100;
-            loanApproved = maxPayment > firstPayment;
-
-            // Выводим результат
-            System.out.printf("Максимальный платеж при ЗП %d равен %.2f рублей. Платеж по кредиту %.2f рублей. "
-                    + ((loanApproved) ? "Одобрено." : "Отказано.") + "\n", salary, maxPayment, firstPayment);
         }
     }
 }
